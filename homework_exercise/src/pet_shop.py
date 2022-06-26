3f8b2979f273fd4756a7458d9bda69f934a26155
Nathaniel Forsyth <ironsideforsyth@gmail.com>
2022-06-26 15:04:54 +0100

    test 9 passed

--- homework_exercise/src/pet_shop.py
+++ homework_exercise/src/pet_shop.py
@@ -23,3 +23,10 @@ def get_pets_by_breed(shop, breed):
         if pet["breed"] == breed:
             breed_pets.append(pet)
     return breed_pets
+
+def find_pet_by_name(shop, name):
+    named_pet = {}
+    for pet in shop["pets"]:
+        if pet["name"] == name:
+            named_pet.update(pet)
+    return named_pet
